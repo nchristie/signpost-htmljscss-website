fcba091c5ef3591ba3611b833af599b38ae5d555
Naomi Christie <naomi.christie@gmail.com>
2023-03-12 20:25:00 +0000

    Get a bit of styling and substance in place

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,5 +1,24 @@
 const button = document.querySelector('button');
+
 button.addEventListener('click', () => {
-  console.log('Button clicked!');
-  alert('Button clicked!');
+  const writtenTerm = document.querySelector('#written-term-input').value;
+  const category = document.querySelector('#category-select').value;
+  const writtenLanguage = document.querySelector('#language-select').value;
+
+  const url = `http://localhost:3001/api/v1/video_urls/find_urls?written_term=${writtenTerm}&category=${category}&written_language=${writtenLanguage}`;
+
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      // Do something with the data returned by the API
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
 });
